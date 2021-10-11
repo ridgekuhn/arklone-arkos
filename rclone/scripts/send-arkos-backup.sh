877060652b658aa039436442e1fe7301f91e4d21
ridgek <ridgekuhn@users.noreply.github.com>
2021-10-10 23:12:30 -0400

    FIXUP Issue not finding ArkOS backup script on RGB10, RK2020. Closes #1

--- rclone/scripts/send-arkos-backup.sh
+++ rclone/scripts/send-arkos-backup.sh
@@ -8,6 +8,20 @@
 [ ${#ARKLONE[@]} -gt 0 ] || source "/opt/arklone/config.sh"
 [ "$(type -t arkloneLogger)" = "function" ] || source "${ARKLONE[installDir]}/functions/arkloneLogger.sh"
 
+# Find the ArkOS backup script
+BACKUP_SCRIPT=""
+
+if [ -f "/opt/system/Advanced/Backup Settings.sh" ]; then
+	BACKUP_SCRIPT="/opt/system/Advanced/Backup Settings.sh"
+
+elif [ -f "/opt/system/Advanced/Backup ArkOS Settings.sh" ]; then
+	BACKUP_SCRIPT="/opt/system/Advanced/Backup ArkOS Settings.sh"
+
+else
+	echo "ERROR: Could not find ArkOS backup script!"
+	exit 1
+fi
+
 # Use same log as "/opt/system/Advanced/Backup Settings.sh"
 arkloneLogger "${ARKLONE[backupDir]}/arkosbackup.log"
 
@@ -23,7 +37,7 @@ echo "------------------------------------------------------"
 
 # Run normal ArkOS settings backup script
 echo "Backing up your ArkOS settings..."
-. "/opt/system/Advanced/Backup Settings.sh"
+"${BACKUP_SCRIPT}"
 
 if [ $? != 0 ]; then
 	printf "\nCould not create backup file! Exiting...\n"
@@ -31,6 +45,7 @@ if [ $? != 0 ]; then
 else
 	# Sync backup to cloud
 	echo "Sending ArkOS backup to ${ARKLONE[remote]}"
+
 	rclone copy "${ARKLONE[backupDir]}/" "${ARKLONE[remote]}:ArkOS/" -v --filter "+ arkosbackup*" --filter "- *" --config "${ARKLONE[rcloneConf]}"
 fi
 
