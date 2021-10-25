af98e3fd473b09b427e44642a48b404c8bc709d4
ridgek <ridgekuhn@users.noreply.github.com>
2021-10-25 03:48:22 -0400

    Add comment/uncomment support to functions/editConfig

--- functions/editConfig.sh
+++ functions/editConfig.sh
@@ -5,20 +5,23 @@
 
 # Edit an option setting in a config file
 #
-# Where config values are stored like:
+# Where config values are stored in a newline-delimited file as:
 # 'someSetting = "someValue"''
-# without single-quotes, one per line
 #
 # @param $1 {string} The name of the option
 #
 # @param $2 {string} The new value to save
 #
-# @param $3 {string} Path of the file to edit
+# @param [$3] {boolean} Comment/disable setting
+#
+# @param ${@: -1} {string} Path to the config file to edit
 function editConfig() {
     local option="${1}"
     local newVal="${2}"
-    local cfg="${3}"
+    local comment="$([[ "${3}" = "true" ]] && echo "# ")"
+    local cfg="${@: -1}"
 
-    sed -i 's|^'${option}'.*=.*|'${option}' = "'${newVal}'"|' "${3}"
+    # @todo why don't non-capturing groups work? eg, (?: *#)
+    sed -i -E "s|^( *#)? *${option}.*=.*|${comment}${option} = \"${newVal}\"|" "${cfg}"
 }
 
